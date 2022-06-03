691c6664f694ebd434ec6e048a6e33d423b0ab85
Mahabub Sunny <mahabubsany2@gmail.com>
2022-06-03 22:26:46 +0600

    Update

--- Config/index.js
+++ Config/index.js
@@ -6,5 +6,8 @@ Config.NODE_ENV = process.env.NODE_ENV || "development";
 Config.SIM_URL = process.env.SIM_URL;
 Config.SIM_ID = process.env.SIM_ID;
 Config.SIM_TOKEN = process.env.SIM_TOKEN;
+Config.BEU_URL = process.env.BEU_URL;
+Config.BEU_ID = process.env.BEU_ID;
+Config.BEU_TOKEN = process.env.BEU_TOKEN;
 
 module.exports = Config;
--- Routes/Actions.Routes.js
+++ Routes/Actions.Routes.js
@@ -2,9 +2,16 @@ const express = require("express");
 const router = express.Router();
 const createError = require("http-errors");
 const response = require("../Logic/index");
-const { SIM_URL, SIM_ID, SIM_TOKEN } = require("../Config/");
+const {
+  SIM_URL,
+  SIM_ID,
+  SIM_TOKEN,
+  BEU_TOKEN,
+  BEU_ID,
+  BEU_URL,
+} = require("../Config/");
 
-router.get("/simulice_frontend", async (req, res, next) => {
+router.get("/sim_frontend", async (req, res, next) => {
   try {
     const { action, workflow_run, repository, sender } = req.body;
     const sending = await response(
@@ -16,6 +23,25 @@ router.get("/simulice_frontend", async (req, res, next) => {
       SIM_ID,
       "FrontEnd",
       SIM_URL
+    );
+    res.status(201).send(sending);
+  } catch (error) {
+    next(createError(500));
+  }
+});
+
+router.get("/beu_admin", async (req, res, next) => {
+  try {
+    const { action, workflow_run, repository, sender } = req.body;
+    const sending = await response(
+      BEU_TOKEN,
+      action,
+      workflow_run,
+      repository,
+      sender,
+      BEU_ID,
+      "FrontEnd",
+      BEU_URL
     );
     res.status(201).send(sending);
   } catch (error) {
